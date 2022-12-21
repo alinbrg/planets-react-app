b199c4fdf3c87c92cb9758796f2d46a929056968
alinbrg <alina.bregvadze@gmail.com>
2022-12-21 22:07:16 +0400

    changed browserrouter to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Planet from "./Routes/Planet";
 import Home from "./Routes/Home";
 
 function App() {
 	return (
-		<BrowserRouter>
+		<HashRouter>
 			<Routes>
 				<Route path="/" element={<Home />} />
 
 				<Route path="/planet/:planetName" element={<Planet />} />
 			</Routes>
-		</BrowserRouter>
+		</HashRouter>
 	);
 }
 
