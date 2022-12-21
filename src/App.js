d6a8df9ebe92fa2e067b36f438e8ba5052b6b635
alinbrg <alina.bregvadze@gmail.com>
2022-12-21 22:17:48 +0400

    updated package json

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Planet from "./Routes/Planet";
 import Home from "./Routes/Home";
 
 function App() {
 	return (
-		<HashRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
+		<Routes>
+			<Route path="/" element={<Home />} />
 
-				<Route path="/planet/:planetName" element={<Planet />} />
-			</Routes>
-		</HashRouter>
+			<Route path="/planet/:planetName" element={<Planet />} />
+		</Routes>
 	);
 }
 
